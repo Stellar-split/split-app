e2fbf5cd49db240f73b9923b1392b2cb27d16fe7
Kingsman-99 <emmanuelanalaba@gmail.com>
2026-09-04 22:01:35 +0100

    fix: update next.config.js to use @ducanh2912/next-pwa instead of next-pwa

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 const { withSentryConfig } = require('@sentry/nextjs');
-const withPWA = require('next-pwa')({
+const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
   disable: process.env.NODE_ENV === 'development',
 });
